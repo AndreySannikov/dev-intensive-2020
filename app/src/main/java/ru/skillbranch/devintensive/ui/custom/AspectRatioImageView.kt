f9f2bbee690304177da98fab582df30d9d2b8011
shmaks0 <shmelyov.maksim@gmail.com>
2019-09-17 23:36:13 +0300

    hometask 5: fix Linter issues

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AspectRatioImageView.kt
@@ -2,14 +2,14 @@ package ru.skillbranch.devintensive.ui.custom
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
 import ru.skillbranch.devintensive.R
 
 class AspectRatioImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-): ImageView(context, attrs, defStyleAttr) {
+) : AppCompatImageView(context, attrs, defStyleAttr) {
     companion object {
         private const val DEFAULT_ASPECT_RATIO = 1.78F
     }
@@ -19,7 +19,8 @@ class AspectRatioImageView @JvmOverloads constructor(
     init {
         if (attrs != null) {
             val a = context.obtainStyledAttributes(attrs, R.styleable.AspectRatioImageView)
-            aspectRatio = a.getFloat(R.styleable.AspectRatioImageView_aspectRatio, DEFAULT_ASPECT_RATIO)
+            aspectRatio =
+                a.getFloat(R.styleable.AspectRatioImageView_aspectRatio, DEFAULT_ASPECT_RATIO)
             a.recycle()
         }
     }
